9f42f9090595bc67424fe5fdcefc6b1c391e1778
musa_qo76 <musa_qo76@STHQ0126-08>
2024-07-04 14:47:10 +0400

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,46 @@
+﻿class Proqram
+{
+
+    public static int Toplama( int x, int y)
+    {
+        return x + y;
+    }
+    public static int Cixma(int x, int y)
+    {
+        return x - y;
+    }
+    public static int Vurma(int x, int y)
+    {
+        return x * y;
+    }
+    public static int Bolme(int x, int y)
+    {
+        return x / y;
+    }
+    static void Main(string[] args)
+    {
+        Func<int, int,int> func = Toplama;
+        Console.WriteLine("Seciminizi daxil edin:");
+        int secim=int.Parse(Console.ReadLine());
+        if (secim == 1)
+        {
+            func += Toplama;
+            Console.WriteLine(func.Invoke(6, 3));
+        }
+        else if (secim == 2)
+        {
+            func += Cixma;
+            Console.WriteLine(func.Invoke(6, 3));
+        }
+        else if (secim == 3)
+        {
+            func += Bolme;
+            Console.WriteLine(func.Invoke(6, 3));
+        }
+        else if (secim == 4)
+        {
+            func += Vurma;
+            Console.WriteLine(func.Invoke(6, 3));
+        }
+    }
+}
